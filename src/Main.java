b4dbc0ecd7f9fcdede1b75f96db4709112426e88
Nghĩa <98103049+NghiaLe128@users.noreply.github.com>
2023-11-08 23:44:48 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import javax.swing.UIManager;
+
 public class Main {
 
     public static void main(String[] args) {
+        try { 
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel"); 
+        } catch(Exception ignored){}
         new MenuController(); 
     }
 }
